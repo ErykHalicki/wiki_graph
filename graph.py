34bc72528688bc93b6c4cd71296d9d4f5a47274c
Eryk <erykhalicki0@gmail.com>
2024-11-04 11:02:32 -0800

    implementing a*

--- graph.py
+++ graph.py
@@ -16,13 +16,13 @@ class graph:
         self.node_list = []
         self.last_write_index = 0
 
-    def bfs(self, max_depth):
+    def bfs(self, node, max_depth):
         visited = set()
         unvisited_set = set()
         unvisited = deque()
-        unvisited.append(self.root)
+        unvisited.append(node)
         iteration = 0
-        while (unvisited[0].depth < max_depth
+        while (unvisited[0].depth <= max_depth
                and iteration < self.max_iteration):
             # removes the next page to be explored from the unvisited queue
             def pop_and_fetch_neighbors():
@@ -37,7 +37,7 @@ class graph:
                 unvisited.extend(unvisited_neighbors)
                 unvisited_neighbor_links = [neighbor.link for neighbor in unvisited_neighbors]
                 unvisited_set.update(unvisited_neighbor_links)
-                print(f"{current_page.link}")
+                #print(f"{current_page.link}")
                 # print(f"Depth: {current_page.depth}")
                 # print(f"\t{len(unvisited_neighbors)} / {len(current_page.neighbors)} or {100.0 * len(unvisited_neighbors)/len(current_page.neighbors)}%  neighbors are unvisited in current page")
 
@@ -47,8 +47,68 @@ class graph:
 
             pool.shutdown(wait=True)
             iteration += self.max_workers
-            print(f"\tLength of unvisited page list at iteration {iteration} is {len(unvisited)}")
-        print("Done BFS")
+            #print(f"\tLength of unvisited page list at iteration {iteration} is {len(unvisited)}")
+        #print("Done BFS")
+
+    def heuristic(self, node, target):
+        #this function calculates the estiamted distance between 2 concepts
+        #if the return value is low, the 2 links are similar, so we should continue out search in that direction
+
+        #using inverse jaccard similarity of the neighboring node links
+
+        
+
+        node_links, target_links = set([neighbor.link for neighbor in node.neighbors]), set([neighbor.link for neighbor in target.neighbors])
+        intersection = node_links.intersection(target_links)
+        union = node_links.union(target_links)
+        #since we are using inverse jaccard similarity, a high similarity results in a lower score, i.e a lower distance
+        return len(union) / len(intersection) / 1000. if intersection else 1
+
+    def a_star(self, start_link, target_link):#node link is the target link
+        #same steps as dijkstra's, but add heuristic, and make it online
+
+        target = node(target_link)
+        target.get_all_neighbors()
+
+        start = node(start_link)
+        start.distance = 0
+
+        visited = set()
+        heap = list()
+
+        heapq.heappush(heap,start)
+
+        current = start
+
+        while (current.link != target_link
+               and heap != []
+               and current.distance != 10000000):
+            current = heapq.heappop(heap)
+            print(f"{current.link} Distance: {current.distance}")
+            if (current in visited):
+                continue
+            current.get_all_neighbors()
+            for neighbor in current.neighbors:
+                if (current.distance + 1 < neighbor.distance):
+                    print(f"Evaluating {current.link} -> {neighbor.link}")
+                    neighbor.get_all_neighbors()
+                    neighbor.distance = current.distance + 1 + self.heuristic(neighbor, target)
+                    neighbor.parent = current
+                    heapq.heappush(heap, neighbor)
+            visited.add(current)
+
+        path = []
+        if (current.link == target_link):
+            print("found target node!")
+            while (current != self.root):
+                path.append(current.link)
+                current = current.parent
+            path.append(self.root.link)
+            path.reverse()
+        else:
+            print("couldnt find target node!")
+        return path
+
 
     def dijkstras(self, node_link):
         # create a set of unvisited nodes
@@ -124,11 +184,22 @@ class graph:
             print(f"Couldnt find node {self.root.link}, defaulting to {self.node_list[0].link} for root")
             self.root = self.node_list[0]
 
+
 if __name__ == "__main__":
     g = graph(start_link=sys.argv[1], max_workers=50)
     #g.reconstruct("Life.json")
-    g.bfs(8)
-    g.save(f"{g.root.link}.json")
-    path = g.search(sys.argv[2])
-    print(path)
+    #g.bfs(8)
+    #g.save(f"{g.root.link}.json")
+    #path = g.search(sys.argv[2])
+    #print(path)
+
+    g.a_star(sys.argv[1], sys.argv[2])
+
+    start = node(sys.argv[1])
+    target = node(sys.argv[2])
+
+    start.get_all_neighbors()
+    target.get_all_neighbors()
+
+    print(f"distance between {sys.argv[1]} and {sys.argv[2]} is {g.heuristic(start, target)}")
 
